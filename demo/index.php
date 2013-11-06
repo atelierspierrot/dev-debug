1f3dd52b6a5463a0686815e611c40b712fe7567c
Piero Wbmstr <me@e-piwi.fr>
2013-11-06 21:25:36 +0100

    Renaming "PieroWbmstr" in lowercase

--- demo/index.php
+++ demo/index.php
@@ -197,7 +197,7 @@ echo 'require_once __DIR__."/../src/aliases.php";'."\n";
 		    This page is <a href="" title="Check now online" id="html_validation">HTML5</a> & <a href="" title="Check now online" id="css_validation">CSS3</a> valid.
 		</div>
 		<div class="credits float-right">
-		    <a href="https://github.com/atelierspierrot/devdebug">atelierspierrot/devdebug</a> package by <a href="https://github.com/PieroWbmstr">Piero Wbmstr</a> under <a href="http://opensource.org/licenses/GPL-3.0">GNU GPL v.3</a> license.
+		    <a href="https://github.com/atelierspierrot/devdebug">atelierspierrot/devdebug</a> package by <a href="https://github.com/pierowbmstr">Piero Wbmstr</a> under <a href="http://opensource.org/licenses/GPL-3.0">GNU GPL v.3</a> license.
 		</div>
     </footer>
 
